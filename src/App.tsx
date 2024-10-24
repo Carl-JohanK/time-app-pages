e27c40f6142e52075ed9c9f73e6df7106122e711
Carl-Johan <CarlJohank@hotmail.com>
2024-10-24 10:11:07 +0200

    time app first iteration

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [time, setTime] = useState(false);
+
+  function start(){
+    setTime(true);
+    setSec(() => sec + 1);
+  }
+
+  function stop(){
+    setTime(false);
+  }
+
+  /*function clearTime(){
+    
+  }*/
+
+  function timer(){
+    if (time){
+      setTimeout(() => {
+        setSec(() => sec + 1);
+      }, 1000);
+    }
+
+    if(sec == 60){
+      setMin(() => min + 1);
+      setSec(0);
+    }
+  }
+
+  useEffect(() => {
+    timer();
+  }, [sec])
+
+  return (
+    <>
+      <h1>time app</h1>
+      <h2>minutes: {min} secends: {sec}</h2>
+      <h3 onMouseDown={start}>start</h3>
+      <h3 onMouseDown={stop}>stop</h3>
+      {
+      //<h3 onMouseDown={clearTime}></h3>
+      }
+    </>
+  )
+}
+
+export default App
